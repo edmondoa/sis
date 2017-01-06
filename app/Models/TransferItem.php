2c627aa8cf03bf4abe238abe9b0cd8139833880f
Consulting <consulting@threepeaksconsulting.com>
2017-01-06 22:46:04 +0800

    update

--- app/Models/TransferItem.php
+++ app/Models/TransferItem.php
@@ -10,4 +10,25 @@ class TransferItem extends Model
     protected $table = 'stock_transfer_item';  
     protected $fillable = ['transfer_id','product_id','quantity','cost_price'];
     public $timestamps = false;
+    protected $appends = ['product_name','product_code','total'];
+
+
+    public function product()
+    {
+    	return $this->belongsTo('App\Models\Product','product_id','product_id');
+    }
+
+    public function getProductNameAttribute()
+    {
+    	return $this->product->product_name;
+    }
+
+    public function getProductCodeAttribute()
+    {
+    	return $this->product->product_code;
+    }
+    public function getTotalAttribute()
+    {
+    	return $this->product->cost_price * $this->quantity;
+    }
 }
