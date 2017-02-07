39731ebe879ae4f1e278751249ee823560aaaec7
Consulting <consulting@threepeaksconsulting.com>
2017-02-07 17:00:00 +0800

    test

--- public/angular/controllers/transfer.js
+++ public/angular/controllers/transfer.js
@@ -23,7 +23,7 @@
             $("#branch_id_to").val(data.transfer.recv_branch_id).trigger("change");
             // $("#supplier_id").val(data.transfers.supplier_id).trigger("change");
              $("#transfer_id").val(data.transfer.transfer_id)
-            // console.log(data.transfers)
+            console.log(data.transfers)
             if(data.transfer != null)
             {
               $("#stockin-div").attr('disabled',true);
